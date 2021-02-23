c152f3bfc20e16c4ce1e068c2fb144ad494d1447
Fabio Pacifici <mail@fabiopacifici.com>
2021-02-23 11:14:57 +0000

    create posts component and axios request

---
+++ resources/js/components/PostsComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Post title</div>
+
+                    <div class="card-body">
+                        Post content here
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                posts: ""
+            } 
+        },
+
+        mounted() {
+            console.log('Component mounted.');
+            axios.get('api/posts').then(response => {
+                console.log(response.data.data);
+            }).catch(error => {
+                console.log(error);
+            })
+        }
+    }
+</script>
